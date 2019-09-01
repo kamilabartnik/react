fc7a181fb5fe519ae2873f7def465b73bf207a0d
kamilabartnik <kbartnik@g.pl>
2019-09-01 11:20:08 +0200

    login form

--- webs/src/App.js
+++ webs/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PageWrapper from './components/PageWrapper';
+import AdminWrapper from './components/AdminWrapper';
+
 // eslint-disable-next-line
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 //Pages
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Login from './components/pages/Login';
 import Contact from './components/pages/Contact'
 
 class App extends Component {
@@ -14,22 +17,43 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          <PageWrapper>
+          
+          <Route
+            path="/admin"
+            render= {props => (
+              <AdminWrapper>
+                <Login {...props} />
+              </AdminWrapper>
+            )}
+          />
+
+
             <Route
               exact={true}
               path="/"
-              component={Home}
+              render= {props => (
+                <PageWrapper>
+                  <Home {...props} />
+                </PageWrapper>
+              )}
             />
             <Route
               path="/about"
-              component={About}
+              render= {props => (
+                <PageWrapper>
+                  <About {...props} />
+                </PageWrapper>
+              )}
             />
             <Route
               path="/contact"
-              component={Contact}
+              render= {props => (
+                <PageWrapper>
+                  <Contact {...props} />
+                </PageWrapper>
+              )}
             />
 
-          </PageWrapper>
         </Router>
       </div>
     );
---
+++ webs/src/components/pages/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Field from '../Common/Field';
+import { withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const fields = [
+  { name: 'email', elementName: 'input', type: 'email', placeholder: 'Your Email' },
+  { name: 'password', elementName: 'input', type: 'password', placeholder: 'Your Password' },
+]
+
+
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="login-page">
+        <div className="container">
+          <div className="login-form">
+          <div className="row">
+            <h1> Login </h1>  
+          </div>
+            <div className="row">
+              <form onSubmit={this.props.handleSubmit}>
+              {fields.map((f, i) => {
+                return (
+                  <div className="col-md-12">
+                    <Field
+                      key={i}
+                      {...f}
+                      value={this.props.values[f.name]}
+                      name={f.name}
+                      onChange={this.props.handleChange}
+                      onBlur={this.props.handleBlur}
+                      touched={(this.props.touched[fields.name])}
+                      errors={this.props.errors[fields.name]}
+                    />
+                  </div>
+                )
+
+              })}
+                <div className="col-md-12">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withFormik({
+  mapPropsToValues: () => ({
+    email: '',
+    password: ''
+  }),
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email('Email is invalid').required('You need pass email'),
+    password: Yup.string().required('You need pass password')
+  }),
+  handleSubmit: (values, { setSubmitting }) => {
+    console.log("Login attempt", values)
+  }
+})(Login);
