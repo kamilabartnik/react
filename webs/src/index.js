6b476eeb96325421ba5b19c760b7abbd05f98a3d
kamilabartnik <kbartnik@g.pl>
2019-09-02 11:46:21 +0200

    Persisting reducers using localStorage

--- webs/src/index.js
+++ webs/src/index.js
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './store/reducers';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
+import configureStore from './store/cofigureStore';
+import {PersistGate} from 'redux-persist/integration/react';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-  );
+
+const {store, persistor} = configureStore();
 
 ReactDOM.render(
   <Provider store = {store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>, 
   document.getElementById('root'));
 
