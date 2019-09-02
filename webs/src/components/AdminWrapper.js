03e48560cdce52d2ee465ad7717a56eed5033546
kamilabartnik <kbartnik@g.pl>
2019-09-02 10:12:32 +0200

    sidebar hover

--- webs/src/components/AdminWrapper.js
+++ webs/src/components/AdminWrapper.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import "./assets/css/admin.css";
-
 import classNames from 'classnames';
+import Sidebar from './Common/Sidebar';
+
+
 import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -13,6 +15,9 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from '@material-ui/core/Divider';
+
 
 const drawerWidth = 240;
 
@@ -21,30 +26,78 @@ const styles = theme => ({
     paddingRight: 24
   },
   appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+  },
+  appBarShift: {
     marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   drawerPaper: {
     position: 'relative',
     whiteSpace: 'noWrap',
-    width: drawerWidth
+    width: drawerWidth,
+    transition: theme.transitions.create('width',{
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    width: theme.spacing.unit *7,
+    transition: theme.transitions.create('width',{
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar
   }
 })
 
 
 class AdminWrapper extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      open: true
+    }
+  }
+
+  handleDrawerOpen = (e) => {
+    this.setState({open: true});
+  }
+
+  handleDrawerClose = (e) => {
+    this.setState({open: false});
+  }
+
+
 
   render() {
     const {classes} = this.props;
     return (
       <div id="admin-page">
-        <AppBar className={classes.appBar}>
+
+        <AppBar className={classNames(classes.appBar, 
+              this.state.open && classes.appBarShift)}>
           <Toolbar className={classes.toolbar}>
-            <IconButton>
+            <IconButton onClick={this.handleDrawerOpen}>
               <MenuIcon />
             </IconButton>
             <Typography
-              component = "h1"
+              component="h1"
               variant="h6"
               color="inherit"
               noWrap
@@ -54,15 +107,18 @@ class AdminWrapper extends Component {
         </AppBar>
         <Drawer
           classes={{
-            paper:classes.drawerPaper}}
+            paper: classNames(classes.drawerPaper, 
+                !this.state.open && classes.drawerPaperClose)}}
           variant="permanent"
-          opem ={true}
+          open ={true}
           >
-            <List>
-              <ListItem>
-                Dashboard
-              </ListItem>
-            </List>
+            <div className={classes.toolbarIcon}>
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <Divider />
+            <Sidebar />
           </Drawer>
         {this.props.children}
       </div>
---
+++ webs/src/components/Common/Sidebar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+
+import FaceIcon from '@material-ui/icons/Face';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+
+function ListItemLink(props) {
+  return <ListItemLink button component="a" {...props} />;
+}
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <List>
+        <ListItemLink to="/admin">
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+        </ListItemLink>
+        <ListItemLink button to="/post">
+            <ListItemIcon>
+              <FileCopyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Post" />
+        </ListItemLink>
+        <ListItemLink to="/users">
+            <ListItemIcon>
+              <FaceIcon />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+        </ListItemLink>
+      </List>
+    )
+  }
+}
+
+export default Sidebar;
+
