9d51748f5ced2578d0dc7c96ad5536f413402b4a
kamilabartnik <kbartnik@g.pl>
2019-10-26 14:34:20 +0200

    gh pages deploy

---
+++ webs/build/precache-manifest.cb3ff22ef16d14e5ceec739aaff2a374.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14c041e5066e18c981b669438b2dcca",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "58b2bc5fdca9148a4a9f",
+    "url": "/react/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "3b612eab1d95df7ea7bc",
+    "url": "/react/static/css/main.e1f3f99e.chunk.css"
+  },
+  {
+    "revision": "58b2bc5fdca9148a4a9f",
+    "url": "/react/static/js/2.555b4c9f.chunk.js"
+  },
+  {
+    "revision": "3b612eab1d95df7ea7bc",
+    "url": "/react/static/js/main.603af311.chunk.js"
+  },
+  {
+    "revision": "30d6dd3ac54e6e44707b",
+    "url": "/react/static/js/runtime~main.6c77f469.js"
+  },
+  {
+    "revision": "f8648487f068e002a0ad46321d555add",
+    "url": "/react/static/media/01-full.f8648487.jpg"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/react/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/react/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/react/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/react/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/react/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/react/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "150043d17a19751013501f07402c5ab6",
+    "url": "/react/static/media/kotek.150043d1.jpg"
+  }
+]);
